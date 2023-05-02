e7008a90ba7f9a35360c72c0db347fcd24df18e5
Dustin Putnam <dputnam6@uncc.edu>
2023-05-01 23:28:27 -0400

    Got the app running.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 3155GroupProject
+
+## How to run the code
+
+1. Install all of the required libraries from the requirments.txt file by running the following command in your terminal.
+
+```cmd
+pip install -r requirments.txt
+```
+
+2. Run the App.py file int the app directory and go to http://127.0.0.1:5000
--- app/App.py
+++ app/App.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flask import Flask, abort, redirect, render_template, request
 from models import db, User
 from user_repository import user_repo_singleton
@@ -9,39 +13,41 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
 
 db.init_app(app)
 
-@app.get('/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.get('/about')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.get('/clubs')
+@app.route('/clubs')
 def clubs():
     return render_template('clubs.html')
 
-@app.get('/contact')
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-@app.get('/events')
+@app.route('/events')
 def events():
     return render_template('events.html')
 
-@app.get('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    if request.method == 'POST':
+        email = request.form.get('signup-email')
+        name = request.form.get('signup-name')
+        password = request.form.get('signup-password')
+        confirm = request.form.get('signup-confirm')
 
-app.post('/signup')
-def signup():
-    email = request.form.get('signup-email')
-    name = request.form.get('signup-name')
-    password = request.form.get('signup-password')
-    confirm = request.form.get('signup-confirm')
-
-    if(password == confirm):
-        new_user = user_repo_singleton.create_user(name, password, email)
-        return render_template('clubs.html')
+        if(password == confirm):
+            new_user = user_repo_singleton.create_user(name, password, email)
+            return render_template('clubs.html')
+        else:
+            return render_template('signup.html')
     else:
         return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
